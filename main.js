d3e137575e1f6d03588f24981f4e62db69740877
Martin Marinov <m.marinov1733@gmail.com>
2025-07-23 18:55:35 +0300

    бутони за светла / тъмна тема
    Оправих проблема с навигацията при страницата със стажа. Бутоните за смяна на езика остават активни

--- main.js
+++ main.js
@@ -23,5 +23,27 @@ function changeLang(lang) {
   i18next.changeLanguage(lang, () => {
     localStorage.setItem("lang", lang);
     updateContent();
+    updateActiveLanguageButton(lang);
   });
+}
+
+function updateActiveLanguageButton(activeLang) {
+  document.querySelectorAll(".changeLanguageButton").forEach((btn) => {
+    const btnLang = btn.getAttribute("data-lang");
+    if (btnLang === activeLang) {
+      btn.classList.add("activeLanguage");
+    } else {
+      btn.classList.remove("activeLanguage");
+    }
+  });
+}
+
+function setTheme(id) {
+  if (id == "sun") {
+    document.getElementById(id).classList.add("activeTheme");
+    document.getElementById("moon").classList.remove("activeTheme");
+  } else if (id == "moon") {
+    document.getElementById(id).classList.add("activeTheme");
+    document.getElementById("sun").classList.remove("activeTheme");
+  }
 }
